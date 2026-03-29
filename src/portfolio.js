9bdef8b89c71cac248f8fae02db1acdb33e6687f
Junchen-Ding <junchen.jcding@gmail.com>
2026-03-29 20:57:51 +0800

    Update profile and fix layout

--- src/portfolio.js
+++ src/portfolio.js
@@ -351,10 +351,10 @@ const contactInfo = {
   title: emoji("Feel free to contact"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  location: "Sydney / Wollongong, Australia",
   // number: "+92-0000000000",
   email_address: "junchen.ding@unsw.edu.au",
-  avatar_image_path: "mine.jpgjpg"
+  location: "Sydney / Wollongong, Australia",
+  avatar_image_path: "jamisonphoto.jpg"
 };
 
 // Twitter Section
