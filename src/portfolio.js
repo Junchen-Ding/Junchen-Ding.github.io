6b91e7fc01e13dfb1b0b05331757d5afec714e44
Junchen-Ding <junchen.jcding@gmail.com>
2026-06-12 11:40:27 +0800

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -348,7 +348,7 @@ const resumeSection = {
 };
 
 const contactInfo = {
-  title: emoji("Feel free to contact"),
+  title: emoji("Contact me"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
   // number: "+92-0000000000",
